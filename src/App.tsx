610f2c35d4731afbc03a54040cdce3efe11e8b0e
Hal Smith <hal@schnibb.com>
2021-04-04 22:28:02 +0100

    Started moving login into team page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React, { useEffect, useState, useCallback, FC } from 'react';
 import { PlayerMap } from './Player';
+import {Team} from './Team';
 import { TransferMap, TransferList } from './Transfer';
 import { Tabs } from './Tabs';
 import logo from './logo.svg';
 import './App.css';
-
-interface TabItem {
-   name: string,
-   selected: boolean
-}
 
 export type TabCallback = (tab: string) => void;
 
@@ -16,9 +12,9 @@ function App() {
 
    const [tabSelected, setTabSelected] = useState<string>('default');
    const [players, setPlayers] = useState<PlayerMap>();
-   const [transfers, setTransfers] = useState<TransferMap>();
-   const [password, setPassword] = useState('');
-   const [email, setEmail] = useState('');
+   // const [transfers, setTransfers] = useState<TransferMap>();
+   // const [password, setPassword] = useState('');
+   // const [email, setEmail] = useState('');
 
    const setTabCallback = useCallback(
       tab => {
@@ -35,46 +31,41 @@ function App() {
       });
    }, []);
 
-   const handleSubmit = (e: any) => {
-      e.preventDefault();
-      fetch('/api/optimise/login', {
-         method: 'POST',
-         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-         },
-         body: JSON.stringify({
-            'username': email,
-            'password': password
-         })
-      }).then(res => res.json()).then(data => {
-         setTransfers(data.transfers);
-      });
-   }
+   // const handleSubmit = (e: any) => {
+   //    e.preventDefault();
+   //    fetch('/api/optimise/login', {
+   //       method: 'POST',
+   //       headers: {
+   //          'Accept': 'application/json',
+   //          'Content-Type': 'application/json'
+   //       },
+   //       body: JSON.stringify({
+   //          'username': email,
+   //          'password': password
+   //       })
+   //    }).then(res => res.json()).then(data => {
+   //       setTransfers(data.transfers);
+   //    });
+   // }
 
    const tabs = ['default', 'team', 'about'];
 
-   
+
 
    return (
       <div className="App">
          <Tabs setSelected={setTabCallback} tabs={tabs} />
-         <div>{tabSelected}</div>
-         {/* <Tabs>
-            <Tab title="Lemon">Lemon is yellow</Tab>
-            <Tab title="Strawberry">Strawberry is red</Tab>
-            <Tab title="Pear">Pear is green</Tab>
-         </Tabs> */}
+         {tabSelected == 'team' && <Team />}
          <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <form onSubmit={handleSubmit}>
+            {/* <form onSubmit={handleSubmit}>
                <div><input type="text" name="email" placeholder="email" onChange={event => setEmail(event.target.value)} value={email} /></div>
                <div><input type="password" name="password" placeholder="password" onChange={event => setPassword(event.target.value)} value={password} /></div>
                <div><button type="submit">Submit</button></div>
-            </form>
-            <div>
-               {players !== undefined && transfers !== undefined && <TransferList players={players} transfers={transfers} />}
-            </div>
+            </form> */}
+            {/* <div> */}
+               {/* {players !== undefined && transfers !== undefined && <TransferList players={players} transfers={transfers} />} */}
+            {/* </div> */}
          </header>
       </div>
    );
--- src/Team.tsx
+++ src/Team.tsx
@@ -1,6 +1,32 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import {TransferMap} from './Transfer';
 
 
 export const Team: FC = () => {
-   return (<div>Team</div>);
+
+   const [transfers, setTransfers] = useState<TransferMap>();
+   const [password, setPassword] = useState('');
+   const [email, setEmail] = useState('');
+   
+   const handleSubmit = (e: any) => {
+      e.preventDefault();
+      fetch('/api/optimise/login', {
+         method: 'POST',
+         headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+            'username': email,
+            'password': password
+         })
+      }).then(res => res.json()).then(data => {
+         setTransfers(data.transfers);
+      });
+   }
+   return (<form onSubmit={handleSubmit}>
+      <div><input type="text" name="email" placeholder="email" onChange={event => setEmail(event.target.value)} value={email} /></div>
+      <div><input type="password" name="password" placeholder="password" onChange={event => setPassword(event.target.value)} value={password} /></div>
+      <div><button type="submit">Submit</button></div>
+   </form>);
 }
